89f6da2f84cd654f2c228873737af75f0d817fff
scorinnesche <corinneblessing8@gmail.com>
2020-07-05 13:48:59 +0200

    added GeneratorTest, tests ok

--- src/test/java/GeneratorTest.java
+++ src/test/java/GeneratorTest.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GeneratorTest {
+    @Test
+    public void ifNumberGeneratorIsCreated_DifferentNumbersArePresented ()  {
+        Generator generator = new Generator();
+       int generatedNumers=  generator.generateNumbers();
+        assertEquals(9, generatedNumers);
+    }
+
+
 }
