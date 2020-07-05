f1f0a7d0dd9db608d3df5ac447fcc02c259d7bb6
scorinnesche <corinneblessing8@gmail.com>
2020-07-05 13:47:50 +0200

    added Generatorclass, tests ok

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,2 +1,12 @@
+import java.util.Random;
+
 public class Generator {
+   public int generateNumbers()  {
+      int maxNumber = 0;
+      for (int i = 0; i<9; i++)  {
+         maxNumber = maxNumber + 1;
+      }
+   return maxNumber;
+   }
+
 }
